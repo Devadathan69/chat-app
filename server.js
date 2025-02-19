8cb025887448cb672ae77e1c16f6a0362d4a54b7
DevadathanMR <devadathanappu69@gmail.com>
2025-02-19 21:14:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const cors = require("cors");
 const app = express();
 const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
 
 const io = new Server(server, {
   cors: {
