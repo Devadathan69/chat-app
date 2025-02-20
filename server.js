afaf43cc3d2798bf623950c16c01e9a4ffa2d087
DevadathanMR <devadathanappu69@gmail.com>
2025-02-20 23:22:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,51 +2,47 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
-const PORT = process.env.PORT || 3000; // Use Render-assigned PORT
+const PORT = process.env.PORT || 3000;
 
 const io = new Server(server, {
-  cors: {
-    origin: "https://front-benchers-chat.netlify.app", // Allow frontend origin
-  },
-});
-
-app.use(cors());
-app.use(express.static(path.join(__dirname, "public"))); // Serve frontend files
-
-// Serve index.html when visiting "/"
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-const io = require("socket.io")(server, {
     cors: { origin: "*" },
 });
 
-let onlineUsers = [];
+let messages = [];
+let onlineUsers = new Map();
 
 io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
+    console.log("User connected:", socket.id);
+
+    socket.emit("chatHistory", messages);
+
+    socket.on("setUsername", (username) => {
+        onlineUsers.set(socket.id, username);
+        io.emit("onlineUsers", Array.from(onlineUsers.values()));
+    });
 
     socket.on("message", (data) => {
-        io.emit("message", data); // Broadcast message to all clients
+        messages.push(data);
+        io.emit("message", data);
     });
 
     socket.on("typing", (username) => {
         socket.broadcast.emit("typing", username);
     });
 
+    socket.on("stopTyping", () => {
+        socket.broadcast.emit("stopTyping");
+    });
+
     socket.on("disconnect", () => {
-        console.log("User disconnected:", socket.id);
+        onlineUsers.delete(socket.id);
+        io.emit("onlineUsers", Array.from(onlineUsers.values()));
     });
 });
 
-
-
-// Start the server
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
